a3a0485ca7cfd410a942dae463acb62c8ca62dba
Zhuoyang ye <737475845@qq.com>
2024-04-13 12:58:10 -0700

    Change noise.py

---
+++ noise.py
@@ -0,0 +1,27 @@
+# Import from Qiskit Aer noise module
+from qiskit_aer.noise import (NoiseModel, QuantumError, ReadoutError,
+    pauli_error, depolarizing_error, thermal_relaxation_error)
+
+
+
+
+def construct_bitflip_noise_model(p_reset, p_meas, p_gate1):
+    # Example error probabilities
+    p_reset = p_reset
+    p_meas = p_meas
+    p_gate1 = p_gate1
+
+    # QuantumError objects
+    error_reset = pauli_error([('X', p_reset), ('I', 1 - p_reset)])
+    error_meas = pauli_error([('X',p_meas), ('I', 1 - p_meas)])
+    error_gate1 = pauli_error([('X',p_gate1), ('I', 1 - p_gate1)])
+    error_gate2 = error_gate1.tensor(error_gate1)
+
+    # Add errors to noise model
+    noise_bit_flip = NoiseModel()
+    noise_bit_flip.add_all_qubit_quantum_error(error_reset, "reset")
+    noise_bit_flip.add_all_qubit_quantum_error(error_meas, "measure")
+    noise_bit_flip.add_all_qubit_quantum_error(error_gate1, ["u1", "u2", "u3"])
+    noise_bit_flip.add_all_qubit_quantum_error(error_gate2, ["cx"])
+
+    return noise_bit_flip
