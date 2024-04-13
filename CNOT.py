e7c18315b014a1d992ba9f59442412af57e08138
Leo Liu <liuqiyucn@gmail.com>
2024-04-13 19:14:30 -0400

    implement special cases of static circuit vs dynamic circuit

--- CNOT.py
+++ CNOT.py
@@ -3,20 +3,30 @@ import matplotlib.pyplot as plt
 from QuantumAlgorithm import QuantumAlgorithm
 import qiskit as qs
 
-class CNOTCircuit(QuantumAlgorithm):
-    def __init__(self, num_qubits: int) -> None:
-        super().__init__(num_qubits)
 
-    def construct_circuit(self, mode : str) :
-        qc = qs.QuantumCircuit(self.num_qubits)
+class CNOTCircuit(QuantumAlgorithm):
+    def __init__(self, num_qubits: int, mode:str) -> None:
+        super().__init__(num_qubits)
+        self._mode = mode
+    # construct the quantum _circuit
+    # three cases:
+    # case 1: simple cx gate assuming connectivity is not an issue
+    # case 2: using swap gate to implement CNOT (only work for even qubits currently)
+    # case 3: using dynamic circuits to implement remote CNOT (only work for 7 qubit currently)
+    # next step is to generalize to N qubits
+    def construct_circuit(self) :
+        qubits = qs.QuantumRegister(self.num_qubits)
+        cbits = qs.ClassicalRegister(2)
+        qc = qs.QuantumCircuit(qubits, cbits)
         middle = int(self.num_qubits/2)
         qc.x(0)
         qc.barrier()
         # simple cx on the first and last qubit
-        if mode == 'simple':
+        if self._mode == 'simple':
             qc.cx(0, self.num_qubits-1)
             # unitary swap to implement cx on first and last qubit
-        elif mode == 'unitary':
+            qc.measure([0, self.num_qubits-1], [0, 1])
+        elif self._mode == 'unitary':
             for i in range(int(self.num_qubits/2-1)):
                 qc.cx(i, i+1)
                 qc.cx(i+1, i)
@@ -30,8 +40,43 @@ class CNOTCircuit(QuantumAlgorithm):
                 qc.cx(middle - i - 2, middle - i - 1)
                 qc.cx(middle + i, middle + i + 1 )
                 qc.cx(middle + i + 1, middle + i)
-        # elif mode == 'dynamic': 
-        self.qc = qc
+            qc.measure([0, self.num_qubits-1], [0, 1])
+        elif self._mode == 'dynamic': 
+            qc.h(2)
+            qc.h(4)
+            qc.barrier()
+            qc.cx(0,1)
+            qc.cx(2,3)
+            qc.cx(4,5)
+            qc.cx(1,2)
+            qc.cx(3,4)
+            qc.cx(5,6)
+            qc.barrier()
+            qc.h([1,3,5])
+            qc.measure(4,0)
+            with qc.if_test((cbits[0], 1)):
+                qc.x(6)
+            qc.measure(2,1)
+            with qc.if_test((cbits[1], 1)):
+                qc.x(6)
+            qc.measure(5,0)
+            with qc.if_test((cbits[0], 1)):
+                qc.x(3)
+            qc.measure(3,0)
+            with qc.if_test((cbits[0], 1)):
+                qc.x(1)
+            qc.measure(1,0)
+            with qc.if_test((cbits[0], 1)):
+                qc.z(0)
+            qc.measure([0, self.num_qubits-1], [0,1])
+        self._circuit = qc
 
-    def compute_result(self):
-        return 0
+
+
+CNOT = CNOTCircuit(8, 'unitary')
+CNOT.construct_circuit()
+from noise import construct_bitflip_noise_model
+noise_model = construct_bitflip_noise_model(0.01,0.01,0.01)
+CNOT.add_noise_model(noise_model)
+CNOT.show_noise_effect(1000)
+plt.show()
