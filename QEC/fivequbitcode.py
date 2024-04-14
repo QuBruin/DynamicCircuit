2e896dde8c92ffe7dc2071f105a6d3474e10d81f
Zhuoyang ye <737475845@qq.com>
2024-04-14 07:37:35 -0700

    Add encoding/decoding method for five qubit code

--- QEC/fivequbitcode.py
+++ QEC/fivequbitcode.py
@@ -3,27 +3,48 @@ from .QECCode import QECCode
 
 class fivequbitcode(QECCode):
     
-    def __init__(self, physical_qubits: int):
-        super().__init__(physical_qubits)
+    def __init__(self, physical_qubits=5):
+        super().__init__(physical_qubits,4,2)
         self.set_stabilizers(["XZZXI","IXZZX","XIXZZ","ZXIXZ"])
-        #Store the current depth of the circuit during construction
-        #This should be stored to add facknoise.
-        self._corrent_depth=0
-        
 
+      
+    def construct_encoding_circuit(self):
+        self._circuit.z(0)
+        self._circuit.h(2)
+        self._circuit.h(3)
+        self._circuit.sdg(0)
+        self._circuit.cx(2,4)
+        self._circuit.cx(3,1)
+        self._circuit.h(1)
+        self._circuit.cx(3,4)
+        self._circuit.cx(1,0)
+        self._circuit.sdg(2)
+        self._circuit.s(3)
+        self._circuit.sdg(4)
+        self._circuit.s(0)
+        self._circuit.s(1)
+        self._circuit.z(2)
+        self._circuit.cx(4,0)
+        self._circuit.h(4)
         
-    def construct_circuit(self):
-        self.construct_syndrome_table()
         
-    
-    def tanner_graph(self):
-        raise NotImplementedError("Subclasses must implement tanner_graph method.")
-    
-    
-    def decode(self,syndrome:str):
-        pass
-    
-    
-    def syndrome_table(self):
-        return super().syndrome_table()
-    
+    def construct_decoding_circuit(self):
+        self._circuit.h(4)
+        self._circuit.cx(4,0)
+        self._circuit.z(2)
+        self._circuit.s(1)
+        self._circuit.sdg(0)
+        self._circuit.s(4)
+        self._circuit.sdg(3)
+        self._circuit.s(2)
+        self._circuit.cx(1,0)
+        self._circuit.cx(3,4)
+        self._circuit.h(1)
+        self._circuit.cx(3,1)
+        self._circuit.cx(2,4)
+        self._circuit.s(0)
+        self._circuit.h(3)
+        self._circuit.h(2)
+        self._circuit.z(0)
+        
+        
