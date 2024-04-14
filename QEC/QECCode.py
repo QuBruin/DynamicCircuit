7d38fd40d93ac485879c35572a0a8ab225673c60
Zhuoyang ye <737475845@qq.com>
2024-04-13 17:39:57 -0700

    Add plot and save

--- QEC/QECCode.py
+++ QEC/QECCode.py
@@ -11,16 +11,10 @@ from typing import List
 class QECCode:
     
     
-<<<<<<< HEAD
     def __init__(self, num_physical_qubits: int, stabilizer_nums: int,distance:int) -> None:
         self._num_physical_qubits = num_physical_qubits
         self._stabilizer_nums = stabilizer_nums
         self._distance=distance
-=======
-    def __init__(self, num_physical_qubits: int, stabilizer_nums: int) -> None:
-        self._num_physical_qubits = num_physical_qubits
-        self._stabilizer_nums = stabilizer_nums
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
         self._noise_model=None
         self._constructed = False
         self._simulator = AerSimulator()
@@ -28,10 +22,7 @@ class QECCode:
         self._stabilizers=[]
         self._fake_noise={}
         self._symdrome_table={}
-<<<<<<< HEAD
         self._error_table={}
-=======
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
         
            
     def construct_circuit(self) -> NotImplementedError:
@@ -47,7 +38,6 @@ class QECCode:
     def decode(self,syndrome):
         raise NotImplementedError("Subclasses must implement decode method.")
     
-<<<<<<< HEAD
     
     #Add the syndrome measurement circuit.
     def construct_circuit_stabilizer(self,stabilizer:str):
@@ -67,29 +57,23 @@ class QECCode:
         errorstr=self._error_table[syndrome]
         for index in range(0,len(errorstr)):
             if errorstr[index]=="Z":
-                self._circuit.z(index)
+                self._circuit.Z(index)
             elif errorstr[index]=="X":
                 self._circuit.x(index)
         
         
 
-=======
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
     def construct_syndrome_table(self):
         num_errors = 3 ** self._num_physical_qubits
         for i in range(num_errors):
             errorstr = ""
-<<<<<<< HEAD
             tmpdistance=0
-=======
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
             num = i
             for _ in range(self._num_physical_qubits):
                 remainder = num % 3
                 if remainder == 0:
                     errorstr = 'I' + errorstr
                 elif remainder == 1:
-<<<<<<< HEAD
                     tmpdistance+=1
                     errorstr = 'X' + errorstr
                 else:
@@ -100,26 +84,15 @@ class QECCode:
             #The code can only detect error with distance less than or equal to self._distance 
             if tmpdistance>self._distance:
                 continue
-=======
-                    errorstr = 'X' + errorstr
-                else:
-                    errorstr = 'Z' + errorstr
-                num //= 3
-            syndromestr = "" 
-            
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
             for stbindex in range(self._stabilizer_nums):
                 if self.commute(self._stabilizers[stbindex],errorstr):
                     syndromestr+="0"
                 else:
                     syndromestr+="1"
             self._symdrome_table[errorstr]=syndromestr
-<<<<<<< HEAD
             if syndromestr in self._error_table.keys():
                 assert False
             self._error_table[syndromestr]=errorstr
-=======
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
              
     
     def show_syndrome_table(self):
--- QEC/fivequbitcode.py
+++ QEC/fivequbitcode.py
@@ -1,4 +1,4 @@
-from QEC.QECCode import QECCode
+from QECCode import QECCode
 
 
 class fivequbitcode(QECCode):
@@ -12,36 +12,9 @@ class fivequbitcode(QECCode):
         
 
         
-<<<<<<< HEAD
     def construct_circuit(self):
         self.construct_syndrome_table()
         
-=======
-    def construct_circuit(self) -> NotImplementedError:
-        raise NotImplementedError("Subclasses must implement construct_circuit method.")
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
-    
-    
-    
-    #Add the syndrome measurement circuit.
-    def construct_circuit_stabilizer(self,stabilizer:str):
-        for index in range(0,len(stabilizer)):
-            if stabilizer[index]=="Z":
-                self._circuit.cnot(index,index+len(stabilizer))
-            elif stabilizer[index]=="X":
-                self._circuit.h(index+len(stabilizer))
-                self._circuit.cnot(index,index+len(stabilizer))
-                self._circuit.h(index+len(stabilizer))
-        
-        
-<<<<<<< HEAD
-    #Construct the circuit to correct the errors    
-=======
->>>>>>> 47b832a80c1cad44937a6420299ac74f8fd7ad39
-    def construct_correct_circuit(self, syndrome:str):
-        
-        pass
-    
     
     def tanner_graph(self):
         raise NotImplementedError("Subclasses must implement tanner_graph method.")
